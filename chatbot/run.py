8b6ef2ae508e23837a387d241cfb15ce43e4d313
vedantsahai18 <vedantsahai18@gmail.com>
2020-04-25 12:49:30 +0530

    removed unecessary files

--- chatbot/run.py
+++ chatbot/run.py
@@ -4,6 +4,6 @@ import time
 subprocess.Popen('rasa run actions', shell=True,
       stdin=PIPE)
 print("Rasa action server running")
-subprocess.Popen('rasa run -m models --endpoint endpoints.yml --enable-api --cors “*” --debug --log-file out.log ', shell=True,
+subprocess.Popen('rasa run -m models --enable-api --log-file out.log', shell=True,
       stdin=PIPE)
 print("Rasa http api server running")
