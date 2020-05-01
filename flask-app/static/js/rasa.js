62af3bf9dbd04efdb9a3047a98792feb0fa45197
vedantsahai18 <vedantsahai18@gmail.com>
2020-05-01 22:31:39 +0530

    new chatbot interface added

--- flask-app/static/js/rasa.js
+++ flask-app/static/js/rasa.js
@@ -1,56 +1,9 @@
+const user_id =27;
 $(document).ready(function () {
-
-	//Widget Code
-	var user_id = document.getElementById("username").innerHTML;
-	var bot = '<div class="chatCont" id="chatCont">' +
-		'<div class="bot_profile">' +
-		'<div class="close">' +
-		'<img src="/static/images/botAvatar.png" class="bot_p_img" opacity: 1;>' +
-		'</div>' +
-		'</div><!--bot_profile end-->' +
-		'<div id="result_div" class="resultDiv"></div>' +
-		'<div class="chatForm" id="chat-div">' +
-		'<div class="spinner">' +
-		'<div class="bounce1"></div>' +
-		'<div class="bounce2"></div>' +
-		'<div class="bounce3"></div>' +
-		'</div>' +
-		'<input type="text" id="chat-input" autocomplete="on" placeholder="Start Typing here..."' + 'class="form-control bot-txt"/>' +
-		'</div>' +
-		'</div><!--chatCont end-->' +
-
-		'<div class="profile_div">' +
-		'<div class="row">' +
-		'<div class="col-hgt col-sm-offset-2">' +
-		'<img src="/static/images/botAvatar.png" class="img-circle img-profile">' +
-		'</div><!--col-hgt end-->' +
-		'<div class="col-hgt">' +
-		'</div><!--col-hgt end-->' +
-		'</div><!--row end-->' +
-		'</div><!--profile_div end-->';
-
-	$("mybot").html(bot);
-
-	// ------------------------------------------ Toggle chatbot -----------------------------------------------
-	$('.profile_div').click(function () {
-		$('.profile_div').toggle();
-		$('.chatCont').toggle();
-		$('.bot_profile').toggle();
-		$('.chatForm').toggle();
-		document.getElementById('chat-input').focus();
-	});
-
-	$('.close').click(function () {
-		$('.profile_div').toggle();
-		$('.chatCont').toggle();
-		$('.bot_profile').toggle();
-		$('.chatForm').toggle();
-	});
-
-	// on input/text enter--------------------------------------------------------------------------------------
 	$('#chat-input').on('keyup keypress', function (e) {
 		var keyCode = e.keyCode || e.which;
 		var text = $("#chat-input").val();
+		console.log(text);
 		if (keyCode === 13) {
 			if (text == "" || $.trim(text) == '') {
 				e.preventDefault();
@@ -65,7 +18,7 @@ $(document).ready(function () {
 		}
 	});
 
-
+})
 	//------------------------------------------- Call the RASA API--------------------------------------
 	function send(text) {
 		$.ajax({
@@ -101,8 +54,8 @@ $(document).ready(function () {
 			box.style.display = "none";
 			video.src = "";
 		}
-   }
-   function stopMap(){
+	}
+	function stopMap(){
 	let map = document.getElementById("map1");
 	if(map.style.display == "block"){
 		map.style.display = "none";
@@ -110,13 +63,15 @@ $(document).ready(function () {
 	}
 
 
-//------------------------------------ Set bot response in result_div -------------------------------------
+	//------------------------------------ Set bot response in result_div -------------------------------------
 	function setBotResponse(val) {
+		var today = new Date();
+		var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 		setTimeout(async function () {
 
 			if ($.trim(val) == '' || val == 'error') { //if there is no response from bot or there is some error
 				val = 'Sorry I wasn\'t able to understand your Query. Let\' try something else!'
-				var BotResponse = '<p class="botResult">' + val + '</p><div class="clearfix"></div>';
+				var BotResponse = '<div class="d-flex justify-content-start mb-4"><div class="img_cont_msg"><img src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg" class="rounded-circle user_img_msg"></div><div class="msg_cotainer">' + val+'<span class="msg_time">'+ time + '</span></div></div>';//<div class="clearfix"></div>;
 				$(BotResponse).appendTo('#result_div');
 			} else {
 
@@ -124,7 +79,7 @@ $(document).ready(function () {
 				var msg = "";
 				for (var i = 0; i < val.length; i++) {
 					if (val[i].hasOwnProperty("image")) { //check if there are any images
-						msg += '<img  width="240" height="240" src=\"data:image/png;base64,' + val[i].image + '\"><div class="clearfix"></div>';
+						msg += '<div class="d-flex justify-content-start mb-4"><div class="img_cont_msg"><img src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg" class="rounded-circle user_img_msg"></div>'+'<img  width="240" height="240" src=\"data:image/png;base64,' + val[i].image +'\">'+ '<span class="msg_time">'+ time + '</span></div>';//<div class="clearfix"></div>;
 					} else if (val[i].hasOwnProperty("buttons")) {
 						addSuggestion(val[i].buttons);
 					} 
@@ -142,7 +97,7 @@ $(document).ready(function () {
 							dis.style.display = "block";
 						}
 						//check if the custom payload type is "chart"
-                   		 if (val[i].custom.payload == "chart") {
+							if (val[i].custom.payload == "chart") {
 							// sample format of the charts data:
 							// var chartData = { "title": "Leaves", "labels": ["Sick Leave", "Casual Leave", "Earned Leave", "Flexi Leave"], "backgroundColor": ["#36a2eb", "#ffcd56", "#ff6384", "#009688", "#c45850"], "chartsData": [5, 10, 22, 3], "chartType": "pie", "displayLegend": "true" }
 
@@ -158,10 +113,11 @@ $(document).ready(function () {
 							// pass the above variable to createChart function
 							createChart(title, labels, backgroundColor, chartsData, chartType, displayLegend)
 							return;
-                    	}
+						}
 					}
 					else {
-						msg += '<p class="botResult">' + val[i].text + '</p><div class="clearfix"></div>';
+						msg +='<div class="d-flex justify-content-start mb-4"><div class="img_cont_msg"><img src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg" class="rounded-circle user_img_msg"></div><div class="msg_cotainer">' + val[i].text+'<span class="msg_time">'+ time + '</span></div></div>';//<div class="clearfix"></div>;
+						
 					}
 
 				}
@@ -178,7 +134,9 @@ $(document).ready(function () {
 	function setUserResponse(val) {
 		stopVideo()
 		stopMap()
-		var UserResponse = '<p class="userEnteredText">' + val + '</p><div class="clearfix"></div>';
+		var today = new Date();
+		var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+		var UserResponse = '<div class="d-flex justify-content-end mb-4"><div class="msg_cotainer_send">' + val +'<span class="msg_time_send">'+ time+ '</span></div><img src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg" class="rounded-circle user_img_msg"></div></div>';//<div class="clearfix"></div>
 		$(UserResponse).appendTo('#result_div');
 		$("#chat-input").val('');
 		scrollToBottomOfResults();
@@ -230,123 +188,119 @@ $(document).ready(function () {
 	});
 	// Suggestions end -----------------------------------------------------------------------------------------
 
+	//====================================== creating Charts ======================================
 
-});
+	//function to create the charts & render it to the canvas
+	function createChart(title, labels, backgroundColor, chartsData, chartType, displayLegend) {
 
+	//create the ".chart-container" div that will render the charts in canvas as required by charts.js,
+	// for more info. refer: https://www.chartjs.org/docs/latest/getting-started/usage.html
+	var html = '<div class=\"chart-container\"> <span class=\"modal-trigger\" id=\"expand\" title=\"expand\" href=\"#modal1\"><i class=\"fa fa-external-link\" aria-hidden=\"true\"></i></span> <canvas id=\"chat-chart\" ></canvas> </div> <div style=\"margin-top: 2px;margin-bottom: 2px;\"></div>'
+	$(html).appendTo('.chats');
 
-//====================================== creating Charts ======================================
+	//create the context that will draw the charts over the canvas in the ".chart-container" div
+	var ctx = $('#chat-chart');
 
-//function to create the charts & render it to the canvas
-function createChart(title, labels, backgroundColor, chartsData, chartType, displayLegend) {
+	// Once you have the element or context, instantiate the chart-type by passing the configuration,
+	//for more info. refer: https://www.chartjs.org/docs/latest/configuration/
+	var data = {
+		labels: labels,
+		datasets: [{
+			label: title,
+			backgroundColor: backgroundColor,
+			data: chartsData,
+			fill: false
+		}]
+	};
+	var options = {
+		title: {
+			display: true,
+			text: title
+		},
+		layout: {
+			padding: {
+				left: 5,
+				right: 0,
+				top: 0,
+				bottom: 0
+			}
+		},
+		legend: {
+			display: displayLegend,
+			position: "right",
+			labels: {
+				boxWidth: 5,
+				fontSize: 10
+			}
+		}
+	}
 
-    //create the ".chart-container" div that will render the charts in canvas as required by charts.js,
-    // for more info. refer: https://www.chartjs.org/docs/latest/getting-started/usage.html
-    var html = '<div class=\"chart-container\"> <span class=\"modal-trigger\" id=\"expand\" title=\"expand\" href=\"#modal1\"><i class=\"fa fa-external-link\" aria-hidden=\"true\"></i></span> <canvas id=\"chat-chart\" ></canvas> </div> <div style=\"margin-top: 2px;margin-bottom: 2px;\"></div>'
-    $(html).appendTo('.chats');
+	//draw the chart by passing the configuration
+	chatChart = new Chart(ctx, {
+		type: chartType,
+		data: data,
+		options: options
+	});
 
-    //create the context that will draw the charts over the canvas in the ".chart-container" div
-    var ctx = $('#chat-chart');
+	scrollToBottomOfResults();
+	}
 
-    // Once you have the element or context, instantiate the chart-type by passing the configuration,
-    //for more info. refer: https://www.chartjs.org/docs/latest/configuration/
-    var data = {
-        labels: labels,
-        datasets: [{
-            label: title,
-            backgroundColor: backgroundColor,
-            data: chartsData,
-            fill: false
-        }]
-    };
-    var options = {
-        title: {
-            display: true,
-            text: title
-        },
-        layout: {
-            padding: {
-                left: 5,
-                right: 0,
-                top: 0,
-                bottom: 0
-            }
-        },
-        legend: {
-            display: displayLegend,
-            position: "right",
-            labels: {
-                boxWidth: 5,
-                fontSize: 10
-            }
-        }
-    }
+	// on click of expand button, get the chart data from gloabl variable & render it to modal
+	$(document).on("click", "#expand", function() {
 
-    //draw the chart by passing the configuration
-    chatChart = new Chart(ctx, {
-        type: chartType,
-        data: data,
-        options: options
-    });
+	//the parameters are declared gloabally while we get the charts data from rasa.
+	createChartinModal(title, labels, backgroundColor, chartsData, chartType, displayLegend)
+	});
 
-    scrollToBottomOfResults();
-}
+	//function to render the charts in the modal
+	function createChartinModal(title, labels, backgroundColor, chartsData, chartType, displayLegend) {
+	//if you want to display the charts in modal, make sure you have configured the modal in index.html
+	//create the context that will draw the charts over the canvas in the "#modal-chart" div of the modal
+	var ctx = $('#modal-chart');
 
-// on click of expand button, get the chart data from gloabl variable & render it to modal
-$(document).on("click", "#expand", function() {
+	// Once you have the element or context, instantiate the chart-type by passing the configuration,
+	//for more info. refer: https://www.chartjs.org/docs/latest/configuration/
+	var data = {
+		labels: labels,
+		datasets: [{
+			label: title,
+			backgroundColor: backgroundColor,
+			data: chartsData,
+			fill: false
+		}]
+	};
+	var options = {
+		title: {
+			display: true,
+			text: title
+		},
+		layout: {
+			padding: {
+				left: 5,
+				right: 0,
+				top: 0,
+				bottom: 0
+			}
+		},
+		legend: {
+			display: displayLegend,
+			position: "right"
+		},
 
-    //the parameters are declared gloabally while we get the charts data from rasa.
-    createChartinModal(title, labels, backgroundColor, chartsData, chartType, displayLegend)
-});
+	}
 
-//function to render the charts in the modal
-function createChartinModal(title, labels, backgroundColor, chartsData, chartType, displayLegend) {
-    //if you want to display the charts in modal, make sure you have configured the modal in index.html
-    //create the context that will draw the charts over the canvas in the "#modal-chart" div of the modal
-    var ctx = $('#modal-chart');
+	modalChart = new Chart(ctx, {
+		type: chartType,
+		data: data,
+		options: options
+	});
 
-    // Once you have the element or context, instantiate the chart-type by passing the configuration,
-    //for more info. refer: https://www.chartjs.org/docs/latest/configuration/
-    var data = {
-        labels: labels,
-        datasets: [{
-            label: title,
-            backgroundColor: backgroundColor,
-            data: chartsData,
-            fill: false
-        }]
-    };
-    var options = {
-        title: {
-            display: true,
-            text: title
-        },
-        layout: {
-            padding: {
-                left: 5,
-                right: 0,
-                top: 0,
-                bottom: 0
-            }
-        },
-        legend: {
-            display: displayLegend,
-            position: "right"
-        },
+	}
 
-    }
-
-    modalChart = new Chart(ctx, {
-        type: chartType,
-        data: data,
-        options: options
-    });
-
-}
-
-function sleep(milliseconds) {
-	const date = Date.now();
-	let currentDate = null;
-	do {
-	  currentDate = Date.now();
-	} while (currentDate - date < milliseconds);
-  }
+	function sleep(milliseconds) {
+		const date = Date.now();
+		let currentDate = null;
+		do {
+			currentDate = Date.now();
+		} while (currentDate - date < milliseconds);
+	}
--- new bot/index.js
+++ new bot/index.js
@@ -1,5 +1,306 @@
-	$(document).ready(function(){
-$('#action_menu_btn').click(function(){
-	$('.action_menu').toggle();
-});
+const user_id=27;
+$(document).ready(function () {
+	$('#chat-input').on('keyup keypress', function (e) {
+		var keyCode = e.keyCode || e.which;
+		var text = $("#chat-input").val();
+		console.log(text);
+		if (keyCode === 13) {
+			if (text == "" || $.trim(text) == '') {
+				e.preventDefault();
+				return false;
+			} else {
+				$("#chat-input").blur();
+				setUserResponse(text);
+				send(text);
+				e.preventDefault();
+				return false;
+			}
+		}
 	});
+
+})
+	//------------------------------------------- Call the RASA API--------------------------------------
+	function send(text) {
+		$.ajax({
+			url: `http://localhost:5005/webhooks/rest/webhook`, //  RASA API
+			type: 'POST',
+			contentType: "application/json",
+			data: JSON.stringify({ message: text, sender: user_id }),
+			success: function (data, textStatus, xhr) {
+				console.log(data);
+
+				if (Object.keys(data).length !== 0) {
+					for (i = 0; i < Object.keys(data[0]).length; i++) {
+						if (Object.keys(data[0])[i] == "buttons") { //check if buttons(suggestions) are present.
+							addSuggestion(data[0]["buttons"])
+						}
+					}
+				}
+
+				setBotResponse(data);
+
+			},
+			error: function (xhr, textStatus, errorThrown) {
+				console.log('Error in Operation');
+				setBotResponse('error');
+			}
+		});
+	}
+
+	function stopVideo(){
+		let video = document.getElementById("video");
+		let box = document.getElementById("y2j");
+		if(box.style.display == "block"){
+			box.style.display = "none";
+			video.src = "";
+		}
+	}
+	function stopMap(){
+	let map = document.getElementById("map1");
+	if(map.style.display == "block"){
+		map.style.display = "none";
+		}
+	}
+
+
+	//------------------------------------ Set bot response in result_div -------------------------------------
+	function setBotResponse(val) {
+		var today = new Date();
+		var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+		setTimeout(async function () {
+
+			if ($.trim(val) == '' || val == 'error') { //if there is no response from bot or there is some error
+				val = 'Sorry I wasn\'t able to understand your Query. Let\' try something else!'
+				var BotResponse = '<div class="d-flex justify-content-start mb-4"><div class="img_cont_msg"><img src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg" class="rounded-circle user_img_msg"></div><div class="msg_cotainer">' + val+'<span class="msg_time">'+ time + '</span></div></div>';//<div class="clearfix"></div>;
+				$(BotResponse).appendTo('#result_div');
+			} else {
+
+				//if we get message from the bot succesfully
+				var msg = "";
+				for (var i = 0; i < val.length; i++) {
+					if (val[i].hasOwnProperty("image")) { //check if there are any images
+						msg += '<div class="d-flex justify-content-start mb-4"><div class="img_cont_msg"><img src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg" class="rounded-circle user_img_msg"></div><div class="msg_cotainer">'+'<img  width="240" height="240" src=\"data:image/png;base64,' + val[i].image + '<span class="msg_time">'+ time + '</span></div></div>';//<div class="clearfix"></div>;
+					} else if (val[i].hasOwnProperty("buttons")) {
+						addSuggestion(val[i].buttons);
+					} 
+					else if (val[i].hasOwnProperty("custom")) {
+						if (val[i].custom.payload == "video") {
+							url = (val[i].custom.data);
+							vid = document.getElementById("video");
+							dis = document.getElementById("y2j");
+							dis.style.display = "block";
+							vid.src = url;
+						}
+						if (val[i].custom.payload == "map") {
+							dis = document.getElementById("map1");
+							await startMap();
+							dis.style.display = "block";
+						}
+						//check if the custom payload type is "chart"
+							if (val[i].custom.payload == "chart") {
+							// sample format of the charts data:
+							// var chartData = { "title": "Leaves", "labels": ["Sick Leave", "Casual Leave", "Earned Leave", "Flexi Leave"], "backgroundColor": ["#36a2eb", "#ffcd56", "#ff6384", "#009688", "#c45850"], "chartsData": [5, 10, 22, 3], "chartType": "pie", "displayLegend": "true" }
+
+							//store the below parameters as global variable, 
+							// so that it can be used while displaying the charts in modal.
+							chartData = (val[i].custom.data)
+							title = chartData.title;
+							labels = chartData.labels;
+							backgroundColor = chartData.backgroundColor;
+							chartsData = chartData.chartsData;
+							chartType = chartData.chartType;
+							displayLegend = chartData.displayLegend;
+							// pass the above variable to createChart function
+							createChart(title, labels, backgroundColor, chartsData, chartType, displayLegend)
+							return;
+						}
+					}
+					else {
+						msg +='<div class="d-flex justify-content-start mb-4"><div class="img_cont_msg"><img src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg" class="rounded-circle user_img_msg"></div><div class="msg_cotainer">' + val[i].text+'<span class="msg_time">'+ time + '</span></div></div>';//<div class="clearfix"></div>;
+						
+					}
+
+				}
+				BotResponse = msg;
+				$(BotResponse).appendTo('#result_div').hide().fadeIn(1000);
+			}
+			scrollToBottomOfResults();
+			hideSpinner();
+		}, 500);
+	}
+
+
+	//------------------------------------- Set user response in result_div ------------------------------------
+	function setUserResponse(val) {
+		//stopVideo()
+		//stopMap()
+		var today = new Date();
+		var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+		var UserResponse = '<div class="d-flex justify-content-end mb-4"><div class="msg_cotainer_send">' + val +'<span class="msg_time_send">'+ time+ '</span></div><img src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg" class="rounded-circle user_img_msg"></div></div>';//<div class="clearfix"></div>
+		$(UserResponse).appendTo('#result_div');
+		$("#chat-input").val('');
+		scrollToBottomOfResults();
+		showSpinner();
+		$('.suggestion').remove();
+	}
+
+
+	//---------------------------------- Scroll to the bottom of the results div -------------------------------
+	function scrollToBottomOfResults() {
+		var terminalResultsDiv = document.getElementById('result_div');
+		terminalResultsDiv.scrollTop = terminalResultsDiv.scrollHeight;
+	}
+
+
+	//---------------------------------------- Spinner ---------------------------------------------------
+	function showSpinner() {
+		$('.spinner').show();
+	}
+
+	function hideSpinner() {
+		$('.spinner').hide();
+	}
+
+
+
+
+	//------------------------------------------- Buttons(suggestions)--------------------------------------------------
+	function addSuggestion(textToAdd) {
+		setTimeout(function () {
+			var suggestions = textToAdd;
+			var suggLength = textToAdd.length;
+			$('<p class="suggestion"></p>').appendTo('#result_div');
+			// Loop through suggestions
+			for (i = 0; i < suggLength; i++) {
+				$('<span class="sugg-options">' + suggestions[i].title + '</span>').appendTo('.suggestion');
+			}
+			scrollToBottomOfResults();
+		}, 1000);
+	}
+
+
+	// on click of suggestions get value and send to API.AI
+	$(document).on("click", ".suggestion span", function () {
+		var text = this.innerText;
+		setUserResponse(text);
+		send(text);
+		$('.suggestion').remove();
+	});
+	// Suggestions end -----------------------------------------------------------------------------------------
+
+	//====================================== creating Charts ======================================
+
+	//function to create the charts & render it to the canvas
+	function createChart(title, labels, backgroundColor, chartsData, chartType, displayLegend) {
+
+	//create the ".chart-container" div that will render the charts in canvas as required by charts.js,
+	// for more info. refer: https://www.chartjs.org/docs/latest/getting-started/usage.html
+	var html = '<div class=\"chart-container\"> <span class=\"modal-trigger\" id=\"expand\" title=\"expand\" href=\"#modal1\"><i class=\"fa fa-external-link\" aria-hidden=\"true\"></i></span> <canvas id=\"chat-chart\" ></canvas> </div> <div style=\"margin-top: 2px;margin-bottom: 2px;\"></div>'
+	$(html).appendTo('.chats');
+
+	//create the context that will draw the charts over the canvas in the ".chart-container" div
+	var ctx = $('#chat-chart');
+
+	// Once you have the element or context, instantiate the chart-type by passing the configuration,
+	//for more info. refer: https://www.chartjs.org/docs/latest/configuration/
+	var data = {
+		labels: labels,
+		datasets: [{
+			label: title,
+			backgroundColor: backgroundColor,
+			data: chartsData,
+			fill: false
+		}]
+	};
+	var options = {
+		title: {
+			display: true,
+			text: title
+		},
+		layout: {
+			padding: {
+				left: 5,
+				right: 0,
+				top: 0,
+				bottom: 0
+			}
+		},
+		legend: {
+			display: displayLegend,
+			position: "right",
+			labels: {
+				boxWidth: 5,
+				fontSize: 10
+			}
+		}
+	}
+
+	//draw the chart by passing the configuration
+	chatChart = new Chart(ctx, {
+		type: chartType,
+		data: data,
+		options: options
+	});
+
+	scrollToBottomOfResults();
+	}
+
+	// on click of expand button, get the chart data from gloabl variable & render it to modal
+	$(document).on("click", "#expand", function() {
+
+	//the parameters are declared gloabally while we get the charts data from rasa.
+	createChartinModal(title, labels, backgroundColor, chartsData, chartType, displayLegend)
+	});
+
+	//function to render the charts in the modal
+	function createChartinModal(title, labels, backgroundColor, chartsData, chartType, displayLegend) {
+	//if you want to display the charts in modal, make sure you have configured the modal in index.html
+	//create the context that will draw the charts over the canvas in the "#modal-chart" div of the modal
+	var ctx = $('#modal-chart');
+
+	// Once you have the element or context, instantiate the chart-type by passing the configuration,
+	//for more info. refer: https://www.chartjs.org/docs/latest/configuration/
+	var data = {
+		labels: labels,
+		datasets: [{
+			label: title,
+			backgroundColor: backgroundColor,
+			data: chartsData,
+			fill: false
+		}]
+	};
+	var options = {
+		title: {
+			display: true,
+			text: title
+		},
+		layout: {
+			padding: {
+				left: 5,
+				right: 0,
+				top: 0,
+				bottom: 0
+			}
+		},
+		legend: {
+			display: displayLegend,
+			position: "right"
+		},
+
+	}
+
+	modalChart = new Chart(ctx, {
+		type: chartType,
+		data: data,
+		options: options
+	});
+
+	}
+
+	function sleep(milliseconds) {
+		const date = Date.now();
+		let currentDate = null;
+		do {
+			currentDate = Date.now();
+		} while (currentDate - date < milliseconds);
+	}
