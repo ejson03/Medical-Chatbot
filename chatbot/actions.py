bfcf272d9340a0f15e4b08de33b30e4f78fa2270
ejson03 <elvisejsondsouza57@gmail.com>
2020-05-07 12:31:41 +0530

    recovered deleted action

---
+++ chatbot/actions.py
@@ -0,0 +1,146 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import unicode_literals
+from __future__ import print_function
+
+from typing import Dict, Text, Any, List, Union, Type, Optional
+
+import typing
+import logging
+from modules.utils import *
+from modules.diagnose import encode_symptom, create_illness_vector, get_diagnosis
+from modules.scrapper import *
+import os
+import requests
+from os import environ
+import uuid
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import SlotSet, AllSlotsReset, EventType
+from rasa_sdk.forms import FormAction, REQUESTED_SLOT
+from rasa_sdk.executor import CollectingDispatcher
+
+from datetime import datetime, date, time, timedelta
+
+
+class ActionGetSong(Action):
+
+    def name(self):
+        return "action_get_song"
+
+    def run(self, dispatcher, tracker, domain):
+        emotion = None
+        entities = tracker.latest_message['entities']
+        for entity in entities:
+            if entity['entity'] == "emotion":
+                emotion = entity['value']
+        if emotion:
+            url = get_music(emotion)
+            dispatcher.utter_message("Here is something for your mood.")
+            dispatcher.utter_message(json_message={"payload":"video","data":url})
+        else:
+            dispatcher.utter_message("I couldnt contemplate what you are going thorugh. I'm sorry.")
+
+class ActionGetQuote(Action):
+    def name(self):
+        return "action_get_quote"
+
+    def run(self, dispatcher, tracker, domain):
+        image = get_quotes()
+        dispatcher.utter_message(image=image)
+
+class ActionShowMap(Action):
+    def name(self):
+        return "action_show_map"
+
+    def run(self, dispatcher, tracker, domain):
+        dispatcher.utter_message(json_message={"payload":"map"})
+        #dispatcher.utter_message("I couldnt contemplate what you are going thorugh. I'm sorry.")
+
+class ActionGetJoke(Action):
+    def name(self):
+        return "action_get_joke"
+
+    def run(self, dispatcher, tracker, domain):
+        joke = get_jokes()
+        dispatcher.utter_message(f"{joke}")
+
+'''Get "action_weather" data'''
+class ActionWeather(Action):
+    def name(self):
+        return 'action_weather'
+
+    def run(self, dispatcher, tracker, domain):
+        app_id = environ.get("WEATHER_ID")
+        app_key = environ.get("WEATHER_KEY")
+        base_url = "https://weather.api.here.com/weather/1.0/report.json?" 
+ 
+        location = tracker.get_slot('location')
+        Final_url = base_url + "app_id=" + app_id + "&app_code=" + app_key + "&product=observation&name=" + location 
+        weather_data = requests.get(Final_url).json()
+
+        if (len(weather_data) > 2):
+            # JSON data works just similar to python dictionary and you can access the value using [].
+            current_temperature =  weather_data['observations']['location'][0]['observation'][0]['temperature']
+            wind=weather_data['observations']['location'][0]['observation'][0]['windSpeed']
+            desc=weather_data['observations']['location'][0]['observation'][0]['description']
+
+            response = """ It is {} in {} at this moment. The temperature is {} degree and the wind speed is {} m/s. """. format(desc, location, current_temperature, wind)
+            dispatcher.utter_message(response)
+        else:
+            dispatcher.utter_message("City Not Found ")
+ 
+
+'''Get "action_temp" data'''
+class ActionTemperature(Action):
+    def name(self):
+        return 'action_temp'
+      
+    def run(self, dispatcher, tracker, domain):
+        app_id = environ.get("WEATHER_ID")
+        app_key = environ.get("WEATHER_KEY")
+        base_url = "https://weather.api.here.com/weather/1.0/report.json?" 
+ 
+        location = tracker.get_slot('location')
+        Final_url = base_url + "app_id=" + app_id + "&app_code=" + app_key + "&product=observation&name=" + location 
+ 
+        weather_data = requests.get(Final_url).json()
+
+        if (len(weather_data) > 2):
+            # JSON data works just similar to python dictionary and you can access the value using [].
+            current_temperature =  weather_data['observations']['location'][0]['observation'][0]['temperature']
+
+            response = """ The temperature in {} is now {} degree currently """. format(location, current_temperature)
+            dispatcher.utter_message(response)
+        else:
+            dispatcher.utter_message("City Not Found ")
+
+class ActionDiagnoseSymptoms(Action):
+
+    def name(self):
+        return "action_diagnose_symptoms"
+
+    def run(self, dispatcher, tracker, domain):
+
+        symptoms = tracker.get_slot("symptom")
+        encoded_symptoms = [encode_symptom(symptom) for symptom in symptoms]
+        illness_vector = create_illness_vector(encoded_symptoms)
+        diagnosis_string = get_diagnosis(illness_vector)
+        dispatcher.utter_message(text=diagnosis_string)
+
+class ActionSymptoms(Action):
+
+    def name(self):
+        return "action_symptoms"
+
+    def run(self, dispatcher, tracker, domain):
+
+        symptoms = tracker.get_slot("symptom")
+        try:  
+            data=get_details(symptoms)
+            dispatcher.utter_message(json_message={"payload":"symptom","data":data})
+        except:
+            dispatcher.utter_message(text="Sorry couldn't find the data for the given symptom.")
+
+
+
