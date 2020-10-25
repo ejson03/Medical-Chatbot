f092109b50029105a29bcebfa60607d22c5cc589
ejson03 <elvisejsondsouza57@gmail.com>
2020-10-25 17:40:37 +0000

    changed action youtube regex

--- actions/modules/utils.py
+++ actions/modules/utils.py
@@ -13,7 +13,7 @@ path = "assets/json"
 def get_url(query):
     query_string = urllib.parse.urlencode({"search_query" : query})
     html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
-    search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
+    search_results = re.findall(r'url\":\"\/watch\?v=(.{11})', html_content.read().decode())
     url = f"https://www.youtube.com/embed/{search_results[0]}?autoplay=1"
     return url
 
