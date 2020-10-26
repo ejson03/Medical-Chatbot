b9c3d97d8d8a2b882913451a51484a9e5fc12f3f
ejson03 <elvisejsondsouza57@gmail.com>
2020-10-26 18:23:09 +0530

    added prune volumes to clean

--- clean.sh
+++ clean.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-docker rmi --force $(docker images)
 docker container rm --force $(docker container ls -aq)
+docker rmi --force $(docker images)
 docker system prune -a -f
+docker volume prune -f
