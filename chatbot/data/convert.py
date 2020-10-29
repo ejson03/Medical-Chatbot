192d28b4ed60bbbfc6c69b0f9d025aa8106954c5
ejson03 <elvisejsondsouza57@gmail.com>
2020-10-29 19:51:14 +0530

    added dataset ignore node

--- chatbot/data/convert.py
+++ chatbot/data/convert.py
@@ -1,7 +1,13 @@
 from rasa.nlu.training_data import load_data
-
-input_training_file = './training.json'
+import os
+import glob
 output_md_file = './nlu.md'
+training = glob.glob("./training_*.json", recursive=True )
 
-with open(output_md_file,'a') as f:
-    f.write(load_data(input_training_file).nlu_as_markdown())
+for file in training:
+    with open(output_md_file,'a') as f:
+        f.write("\n")
+        f.write(load_data(file).nlu_as_markdown())
+    os.remove(file)
+
+
