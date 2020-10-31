415eb915e29a181dafba7be61adcce011da15e94
Pratik Chowdhury <pratikc@live.co.uk>
2020-10-31 20:50:20 +0530

    Add Record Filteration

--- server/lib/utils/filteration.ts
+++ server/lib/utils/filteration.ts
@@ -1,6 +1,4 @@
-// import { UserInterface, RecordInterface, MetadataInterface, PresecriptionInterface } from '../models/user.models';
-// import ruleJudgment from 'rule-judgment';
-
+import { RecordInterface } from '../models/user.models';
 // export const queryFunction = (query: any) => {
 //    for (const [key, value] of Object.entries(query)) {
 //       if (key == 'date') {
@@ -16,5 +14,32 @@
 //    }
 //    return filterList;
 // };
+function filterSingle(query: Partial<RecordInterface>, record: RecordInterface) {
+   if (query.date != null && query.date.getTime() < record.date.getTime()) return false;
+   if (query.height != null && query.height !== record.height) {
+      return false;
+   }
+   if (query.bp != null && query.bp !== record.bp) {
+      return false;
+   }
+   if (query.age != null && query.age !== record.age) {
+      return false;
+   }
+   if (query.weight != null && query.weight !== record.weight) {
+      return false;
+   }
+   if (query.symptoms != null && record.symptoms != null && !record.symptoms.includes(query.symptoms)) {
+      return false;
+   }
+   if (query.allergies != null && record.allergies != null && !record.allergies.includes(query.allergies)) {
+      return false;
+   }
+   if (query.description != null && record.description != null && !record.description.includes(query.description)) {
+      return false;
+   }
+   return true;
+}
 
-// export const filterRecords = (records, query) => {};
+export function filterRecords(records: RecordInterface[], query: Partial<RecordInterface>) {
+   return records.filter(record => filterSingle(query, record));
+}
