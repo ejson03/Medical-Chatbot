87e8887518f0d09c31a82aac5dbcfe736d871b9d
Elvis Dsouza <elvisejsondsouza57@gmail.com>
2020-11-04 15:29:07 +0000

    changed record

--- server/lib/utils/filteration.ts
+++ server/lib/utils/filteration.ts
@@ -15,26 +15,26 @@ import { RecordInterface } from '../models/user.models';
 //    return filterList;
 // };
 function filterSingle(query: Partial<RecordInterface>, record: any) {
-   if (query.date != null && new Date(query.date).getTime() < new Date(record.data.date).getTime()) return false;
-   if (query.height != null && query.height !== record.data.height) {
+   if (query.date != null && new Date(query.date).getTime() == new Date(record.data.date).getTime()) return false;
+   if (query.height != null ) {//&& query.height !== record.data.height) {
       return false;
    }
-   if (query.bp != null && query.bp !== record.data.bp) {
+   if (query.bp != null ) {//&& query.bp !== record.data.bp) {
       return false;
    }
-   if (query.age != null && query.age !== record.data.age) {
+   if (query.age != null){ // && query.age !== record.data.age) {
       return false;
    }
-   if (query.weight != null && query.weight !== record.data.weight) {
+   if (query.weight != null) {//  && query.weight !== record.data.weight) {
       return false;
    }
-   if (query.symptoms != null && record.data.symptoms != null && !record.data.symptoms.includes(query.symptoms)) {
+   if (query.symptoms != null) {// && record.data.symptoms != null && !record.data.symptoms.includes(query.symptoms)) {
       return false;
    }
-   if (query.allergies != null && record.data.allergies != null && !record.data.allergies.includes(query.allergies)) {
+   if (query.allergies != null){ // && record.data.allergies != null && !record.data.allergies.includes(query.allergies)) {
       return false;
    }
-   if (query.description != null && record.data.description != null && !record.data.description.includes(query.description)) {
+   if (query.description != null) {// && record.data.description != null && !record.data.description.includes(query.description)) {
       return false;
    }
    return true;
