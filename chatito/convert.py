82e64f1da1fc63b40a2efb7398434428eff659ff
Pratik Chowdhury <pratikc@live.co.uk>
2021-03-14 19:21:15 +0530

    [Chatito] Add Conversion to RASA MD

---
+++ chatito/convert.py
@@ -0,0 +1,12 @@
+from rasa.nlu.training_data import load_data
+import os
+import glob
+output_md_file = '../nlu.md'
+training = glob.glob("./output/*training.json", recursive=True )
+for file in training:
+    with open(output_md_file,'a') as f:
+        f.write("\n\n")
+        f.write(load_data(file).nlu_as_markdown())
+    os.remove(file)
+
+
