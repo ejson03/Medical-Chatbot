7c6821c07edb731e30c13641be78b7ad335d987f
Pratik Chowdhury <pratikc@live.co.uk>
2021-03-25 15:58:12 +0000

    Add docker dependency run support

---
+++ docker_run_deps/neo4j.sh
@@ -0,0 +1,9 @@
+docker run \
+    --name testneo4j \
+    -p 7474:7474 -p 7687:7687 \
+    -v $HOME/neo4j/data:/data \
+    -v $HOME/neo4j/logs:/logs \
+    -v $HOME/neo4j/import:/var/lib/neo4j/import \
+    -v $HOME/neo4j/plugins:/plugins \
+    --env NEO4J_AUTH=none \
+    neo4j:latest
